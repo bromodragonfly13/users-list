d8d4d466c2b277ac83d7aef040c2f120c7feca4b
Alexandr Shaposhnik <bromodragonfly11@gmail.com>
2022-03-23 19:25:28 +0200

    add delete user modal dialog

--- js/users/delete-user.js
+++ js/users/delete-user.js
@@ -1,6 +1,29 @@
 jQuery(function($){
 
     $(document).on('click', '.delete-user-button', function(){
+
+        var user_id = $(this).attr('data-id');
+
+        $('#mainModal').modal('toggle');
+        $('.modal-dialog').html (`
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="mainModalLabel">Удалить пользователя</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+            <button type="button" class="btn btn-primary" id="remove-button" data-id="`+user_id+`">Удалить</button>
+          </div>
+        </div>
+        `);
+
+    });
+
+    $(document).on('click', '#remove-button', function(){
+
         var user_id = $(this).attr('data-id');
 
         let form_data = new FormData();
@@ -18,11 +41,14 @@ jQuery(function($){
             data : form_data,
             success : function(result) {
                 $('#content_body').empty();
+                $('#mainModal').modal('toggle');
+                $('.modal-dialog').html('');
                 showUsers();
             },
             error: function(xhr, resp, text) {
                 console.log(xhr, resp, text);
             }
         });
+
     });
 });
